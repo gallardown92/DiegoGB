6bae1407ba1d280b07540fadbbe99e2eb3c0c31a
gallardown92 <gallardowndam@gmail.com>
2026-05-03 20:26:41 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,28 @@
-   public static void main(String[] args) {
-        saludarUsuario("Ana", 20);
-        saludarAdmin("Carlos", 35);
-    }
+  //Dentro de Main en DiegoGB
 
-    public static void saludarUsuario(String nombre, int edad) {
-        System.out.println("Hola usuario " + nombre);
-        System.out.println("Edad: " + edad);
+            public static void main(String[] args) {
+                procesarPersona("Usuario", "Ana", 20);
+                procesarPersona("Admin", "Carlos", 35);
+            }
 
-        if (edad >= 18) {
-            System.out.println("Mayor de edad");
-        } else {
-            System.out.println("Menor de edad");
-        }
+            public static void procesarPersona(String tipo, String nombre, int edad) {
+                System.out.println("Hola " + tipo + " " + nombre);
+                System.out.println("Edad: " + edad);
 
-        if (edad > 30) {
-            System.out.println("Usuario mayor");
-        } else {
-            System.out.println("Usuario joven");
-        }
-    }
+                mostrarEdad(edad,tipo);
+            }
+            //Dentro del Main en DiegoGB
+            private static void mostrarEdad(int edad, String tipo) {
+                if (edad >= 18) {
+                    System.out.println("Eres mayor de edad");
+                } else {
+                    System.out.println("Eres menor de edad");
+                }
+                 if (edad > 30){
+                     System.out.println(tipo + " es mayor.");
+                 } else {
+                     System.out.println(tipo + " es joven.");
+                 }
 
-    public static void saludarAdmin(String nombre, int edad) {
-        System.out.println("Hola admin " + nombre);
-        System.out.println("Edad: " + edad);
+                }
 
-        if (edad >= 18) {
-            System.out.println("Mayor de edad");
-        } else {
-            System.out.println("Menor de edad");
-        }
-
-        if (edad > 30) {
-            System.out.println("Admin mayor");
-        } else {
-            System.out.println("Admin joven");
-        }
-    }
-}
